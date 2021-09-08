70b704c419a8a41566360dcd249c4a8b21596d8a
Ehsan Safari <ehsansafari@hotmail.com>
2021-09-08 17:55:33 +0430

    unit testing functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Button from './components/Button'
 import Checkbox from './components/Checkbox';
 import { useState } from "react";
 
+export const replaceCamelWithSpace = (colorName) => {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+}
+
 const App = () => {
   const [checked, setChecked] = useState(false);
   const isChecked = (check) => {
--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,29 @@
 import {render, screen, fireEvent} from '@testing-library/react'
+import { replaceCamelWithSpace } from './App';
 import Button from './components/Button'
 import Checkbox from './components/Checkbox'
 
-test('button has correct initial color', () => {
-    render(<Button />);
-    const button = screen.getByRole('button', {name: 'Change to blue'});
-    expect(button).toHaveStyle({backgroundColor: 'red'});
-    fireEvent.click(button);
-    expect(button).toHaveStyle({backgroundColor: 'blue'});
-    expect(button.textContent).toBe('Change to red');
-});
-
-test('initial conditions', () => {
-    render(<Button />);
-    const button = screen.getByRole('button', {name: 'Change to blue'});
-    expect(button).toBeEnabled();
-    render(<Checkbox />);
-    const checkbox = screen.getByRole('checkbox');
-    expect(checkbox).not.toBeChecked();
+describe('button & checkbox', () => {
+    test('button has correct initial color', () => {
+        render(<Button />);
+        const button = screen.getByRole('button', {name: 'Change to blue'});
+        expect(button).toHaveStyle({backgroundColor: 'red'});
+        fireEvent.click(button);
+        expect(button).toHaveStyle({backgroundColor: 'blue'});
+        expect(button.textContent).toBe('Change to red');
+    });
+    
+    test('initial conditions', () => {
+        render(<Button />);
+        const button = screen.getByRole('button', {name: 'Change to blue'});
+        expect(button).toBeEnabled();
+        render(<Checkbox />);
+        const checkbox = screen.getByRole('checkbox');
+        expect(checkbox).not.toBeChecked();
+    })
 })
 
-test('when checkbox is checked', () => {
+/*test('when checkbox is checked', () => {
     render(<Checkbox />);
     const checkbox = screen.getByRole('checkbox', {name: 'Disable Button'});
     fireEvent.click(checkbox);
@@ -29,4 +32,11 @@ test('when checkbox is checked', () => {
     const button = screen.getByRole('button');
     expect(button).toBeDisabled();
     expect(button).toHaveStyle({backgroundColor: 'gray'});
+})*/
+
+// unit testing functions
+describe('convert camel case colorName to space', () => {
+    test('works for two word camel case', () => {
+        expect(replaceCamelWithSpace('MidnightBlue')).toBe('Midnight Blue')
+    })
 })
